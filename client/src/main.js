a19da2950e122998863df1105e741a42914b3aa4
LAMIYA ALIZADA <lamiyaea@code.edu.az>
2025-10-02 17:34:33 +0400

    fix

---
+++ client/src/main.js
@@ -0,0 +1,26 @@
+import accardions from "./common/Accardion";
+import renderBasket from "./Components/basket";
+import cart from "./Components/cart";
+import { categoryRender, popularProducts, sorting, tagRender } from "./Components/filter";
+import productsRender from "./Components/products";
+import subtotal from "./Components/total";
+
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  accardions();
+  productsRender()
+  categoryRender()
+  popularProducts()
+  sorting()
+  tagRender()
+  cart()
+  renderBasket()
+  subtotal()
+});
